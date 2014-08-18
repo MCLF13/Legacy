fe0650035b6924f76115d9546361a4c299a15ef6
LuizFelipeDG <luizfelipe.dantas@gmail.com>
2014-08-18 17:23:14 -0300

    Library Valuation
    Implementação da Classe FreeCashFlowToFirm

---
+++ FreeCashFlow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreeCashFlow
+{
+    public class FreeCashFlow
+    {    
+        //variaveis referentes ao balanco da empresa para calculo de fundamentos
+        double FCFF;
+        double EBIT;
+        double Depreciation;
+        double Capex;
+        double deltaWC;
+        double taxrate;
+
+        //funcao que calcula o Free Cash Flow to Firm da companhia 
+        public void FreeCashFlowToFirm (double EBIT, double Depreciation, double Capex, double taxrate, double deltaWC)
+        {
+
+            FCFF = EBIT - EBIT*taxrate + Depreciation - Capex - deltaWC;
+        }
+             
+    }
+}
